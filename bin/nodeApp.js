f4041678ccefa0fbe20346f6644332c572391303
saumya <2saumyaray@gmail.com>
2015-01-18 13:44:26 +0530

    done with the example setup

--- bin/nodeApp.js
+++ bin/nodeApp.js
@@ -1,86 +1,45 @@
+// Node uses two kinds of exports while defining a module
+// 1. modules.exports = function(){}
+// 2. exports.Animal = function(){}
 //
 console.log('Application');
 //
-// Example 1 : using the generated single application JS
+/*
+// 1. Example 1 : using the generated single application JS
 var app = require('./hxApp.js');
 console.log('==========================');
 console.log(app);
 console.log('==========================');
 
-// example : 1 : to make new Animal
+// 1.1. : example : 1 : to make new Animal
 var animal_1 = new app.Animal('CAT',9);
-// example : 2 : to make new Animal, save the Constructor as a new "var" and then use it 
+// 1.2. : example : 2 : to make new Animal, save the Constructor as a new "var" and then use it 
 var Animal = app.Animal;
 var animal_2 = new Animal('Tiger',6);
 //lets log
 console.log(animal_1);
 console.log(animal_2);
+*/
 
 //Example 2 : Using individual Class from the single JS
-
-/*
-var o = require('./Jungle');
+var o = require('./Animal');
 var Animal = o.Animal;
-
-console.log(o);
-console.log(o.Animal);
-
-
+//
+var c = new o.Animal('Cat',4);
 var a1 = new Animal('Lion',5);
 var a2 = new Animal('Tiger',8);
 
-console.log(a1);
-console.log(a2);
-
-console.log(a1.getType());
-console.log(a1.getAge());
-console.log('-------------------');
-console.log(a1.age);
-console.log(a1.type);
-
-var c = new o.Animal('Cat',4);
 console.log(c);
-*/
-
-
-
-
-/*
-//Node Way
-
-// Uses modules.exports = function(){}
-//var Animal = require('./Animal');
-
-// Uses only exports.Animal = function(){}
-var o = require('./Animal');
-var a1 = o.Animal('Lion',25);
-var a2 = o.Animal('Tiger',8);
-
 console.log(a1);
-console.log(a1.getType());
-console.log(a1.getAge());
-console.log('-------------------');
-console.log(a1.age);
-console.log(a1.type);
-
 console.log(a2);
-*/
-
-/*
-var Animal = o.Animal;
-
-
-console.log(Animal);
-
-var a1 = new Animal('Lion',5);
-console.log(a1);
-
+console.log('===================');
 console.log(a1.getType());
 console.log(a1.getAge());
-console.log('-------------------');
-console.log(a1.age);
-console.log(a1.type);
-*/
+console.log('===================');
+
+
+
+
 
 
 
